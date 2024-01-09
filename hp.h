df1c16099dcfb5db93c66353d70992cfc33118bc
Tim Arafeh <timarafeh2004@gmail.com>
2024-01-08 19:40:26 -0800

    Health Point System
    Set up basic foundation for the health point system including very basic functions for the damage taken, healing done, etc.  Will work on these further later and make them more detailed.

---
+++ hp.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "hitpointtypes.h"
+
+class hp
+{
+public:
+    // Returns true if set successfully
+    bool setMaxHP(hptype new_max_hp)
+    {
+        if (new_max_hp < 1)
+            return false;
+
+        MaxHP = new_max_hp;
+
+        if (CurrentHP > MaxHP)
+            CurrentHP = MaxHP;
+
+        return true;
+    }
+
+    // Max HP getter
+    hptype getMaxHp()
+    {
+        return MaxHP;
+    }
+
+    // Current HP getter
+    hptype getCurrentHP()
+    {
+        return CurrentHP;
+    }
+
+    void damageTaken(hptype damage)
+    {
+        if (damage > CurrentHP)
+        {
+            CurrentHP = 0;
+            return;
+        }
+
+        CurrentHP -= damage;
+    }
+
+    void healingTaken(hptype heal)
+    {
+        if (heal += CurrentHP > MaxHP)
+        {
+            CurrentHP = MaxHP;
+        }
+
+        CurrentHP += heal;
+    }
+
+private:
+    hptype Shield;
+    hptype CurrentHP;
+    hptype MaxHP;
+};
