22a6ce6c4d87cc7f492493f6dc255ea851da388a
Tim Arafeh <timarafeh2004@gmail.com>
2024-04-19 22:14:40 -0700

    Updated Mage stats
    Just changed a little of how the mages stats increase as they level up.

--- mage.h
+++ mage.h
@@ -2,17 +2,18 @@
 #include "hp.h"
 #include "statblock.h"
 #include "levelup.h"
+
 class Mage : public hp, public StatBlock, public LevelSystem
 
 {
 public:
     static const hptype BASEHP = (hptype)7u;
     static const stattype BASEAD = (stattype)1u;
-    static const stattype BASEAP = (stattype)4u;
+    static const stattype BASEAP = (stattype)7u;
 
     static const hptype HPGROWTH = (hptype)3u;
     static const stattype ADGROWTH = (stattype)1u;
-    static const stattype APGROWTH = (stattype)4u;
+    static const stattype APGROWTH = (stattype)6u;
 
     Mage() : hp(HPGROWTH, HPGROWTH), StatBlock(BASEAD, BASEAP) {}
 
